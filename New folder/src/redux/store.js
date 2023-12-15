bcd6cda533990272f8b6c94d0a5f3f7ddd474332
osamaSale <osama.mohamad.salem@gmail.com>
2023-12-16 01:21:49 +0300

    commit

--- New folder/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import dataSlice from "./slice/slice"
-const store = configureStore({
-  reducer: {
-    data: dataSlice,
-  }
-})
-
-export default store
