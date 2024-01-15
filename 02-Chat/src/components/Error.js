2b9768e11688698dc236bb663c01fbffed1b6357
osamaSale <osama.mohamad.salem@gmail.com>
2024-01-16 02:45:03 +0300

    commit

--- 02-Chat/src/components/Error.js
+++ 02-Chat/src/components/Error.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Error = () => {
+    const location = useLocation();
     return (
         <section>
             <div className="container ">
@@ -15,7 +16,12 @@ export const Error = () => {
                                 <br />
                                 Check out our help center or head back to home.
                             </p>
-                            <Link to="/chat" className="btn btn-primary ms-2">Back to Chat</Link>
+                            {location.pathname === "/signup" || location.pathname === "/passwordReset" ||  location.pathname === "/verifyPassword" || location.pathname === "/" ?
+                            
+                            <Link to="/chat" className="btn btn-primary ms-2">Back to Chat</Link>: 
+                            <Link to="/" className="btn btn-primary ms-2">Back to Signin</Link>
+                        }
+                            
                         </div>
                     </div>
                     <div className="col-12 col-md-5 col-lg-4">
