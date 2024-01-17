3b9734735ece0851c31576a5e9a3635e0948ae8a
osamaSale <osama.mohamad.salem@gmail.com>
2024-01-17 20:26:12 +0300

    commit

--- 01-Store Mobile/src/index.js
+++ 01-Store Mobile/src/index.js
@@ -10,7 +10,8 @@ import store from "./redux/store"
 import { Provider } from "react-redux"
 import axios from 'axios';
 
-axios.defaults.baseURL = `http://localhost:5000/`
+/* axios.defaults.baseURL = `http://localhost:5000/` */
+axios.defaults.baseURL = `https://mobile-store-server-qy6j.onrender.com/`
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
